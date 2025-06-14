e6c7ec826a56168a272d8e976543db8c68555f9d
kajol201 <mdraselislamkajol201@gmail.com>
2025-06-14 08:52:56 +0600

    all voluntier need now section done

--- src/Pages/AllVoluntierNeedPosts/AllVoluntierNeedPosts.jsx
+++ src/Pages/AllVoluntierNeedPosts/AllVoluntierNeedPosts.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import VoluntierSingle from '../../Components/VoluntierNeedNow/VoluntierSingle';
 
 const AllVoluntierNeedPosts = () => {
+    const [vdatas, setData] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3000/allvoluntier')
+            .then(res => res.json())
+            .then(data => {
+
+                setData(data);
+            });
+
+    }, []);
     return (
         <div className='min-h-[calc(100vh-290px)]'>
-            <h1>This is All voluntier need post</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    vdatas.map(singledata => <VoluntierSingle key={singledata._id} singledata={singledata}></VoluntierSingle>)
+                }
+            </div>
         </div>
     );
 };
